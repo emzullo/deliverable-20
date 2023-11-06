c2df16c9adce7528c0c6b5fd8758bfa09773ef4d
emzullo <135267895+emzullo@users.noreply.github.com>
2023-11-06 10:45:04 -0500

    Add files via upload

--- js/video.js
+++ js/video.js
@@ -4,8 +4,6 @@ window.addEventListener("#load", function() {
 	console.log("Good job opening the window")
 	video.autoplay = false;
 	video.loop = false;
-	console.log('Autoplay is set to ' + video.autoplay);
-	console.log('Loop is set to ' + video.loop);
 
 });
 
@@ -13,8 +11,56 @@ document.querySelector("#play").addEventListener("click", function() {
  	console.log("Play Video");
 	 document.querySelector("#volume").innerHTML = video.volume * 100 + '%';
 	 video.play();
-	 console.log("Volume is " + video.volume * 100 + '%');
  });
 
+ document.querySelector("#pause").addEventListener("click", function() {
+	console.log("Pause Video");
+	video.pause();
+});
 
+document.querySelector("#slower").addEventListener("click", function() {
+	video.playbackRate = video.playbackRate * 0.9;
+	console.log("New playback rate:" + video.playbackRate);
+});
 
+document.querySelector("#faster").addEventListener("click", function() {
+	video.playbackRate = video.playbackRate / 0.9;
+	console.log("New playback rate: " + video.playbackRate);
+});
+
+document.querySelector("#skip").addEventListener("click", function() {
+	console.log("Current time: " + video.currentTime);
+	if ((video.currentTime + 10) >= video.duration) {
+		video.currentTime = 0;
+	}
+	else {
+		video.currentTime += 10;
+	}
+	console.log("New time: " + video.currentTime);
+
+});
+
+document.querySelector("#mute").addEventListener("click", function() {
+	if (video.muted == true) {
+		video.muted = false;
+		this.innerHTML = "Mute";
+	}
+	else {
+		video.muted = true;
+		this.innerHTML = "Unmute";
+	}
+
+});
+
+document.querySelector("#slider").addEventListener("click", function() {
+	video.volume = this.value / 100;
+	document.querySelector("#volume").innerHTML = video.volume * 100 + '%';
+});
+
+document.querySelector("#vintage").addEventListener("click", function() {
+	video.classList.add('oldSchool');
+});
+
+document.querySelector("#orig").addEventListener("click", function() {
+	video.classList.remove('oldSchool');
+});
