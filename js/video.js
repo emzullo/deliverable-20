c7cb10ca3c5fba82982e26053193dcdf70643086
emzullo <135267895+emzullo@users.noreply.github.com>
2023-11-05 23:44:31 -0500

    Add files via upload

---
+++ js/video.js
@@ -0,0 +1,12 @@
+var video = document.querySelector('#player1');
+
+window.addEventListener("load", function() {
+	console.log("Good job opening the window")
+	video.autoplay = false;
+	video.loop = false;
+});
+
+// document.querySelector("#play").addEventListener("click", function() {
+// 	console.log("Play Video");
+// });
+
