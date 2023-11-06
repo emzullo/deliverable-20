286b1af994f145bcc91e28a9df0447cc00385d49
emzullo <135267895+emzullo@users.noreply.github.com>
2023-11-05 23:56:03 -0500

    Add files via upload

--- js/video.js
+++ js/video.js
@@ -4,9 +4,15 @@ window.addEventListener("load", function() {
 	console.log("Good job opening the window")
 	video.autoplay = false;
 	video.loop = false;
+	console.log('Autoplay is set to ' + video.autoplay);
+	console.log('Loop is set to ' + video.loop);
+
 });
 
-// document.querySelector("#play").addEventListener("click", function() {
-// 	console.log("Play Video");
-// });
+document.querySelector("#play").addEventListener("click", function() {
+ 	console.log("Play Video");
+	 document.querySelector("#volume").innerHTML = video.volume * 100 + '%';
+	 video.play();
+	 console.log("Volume is " + video.volume * 100 + '%');
+ });
 
